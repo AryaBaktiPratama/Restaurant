437899e2277f6c087b713948a3c1b9ecb884e472
AryaBaktiPratama <arya.pratama@students.amikom.ac.id>
2021-01-12 01:48:18 +0700

    Add project files.

---
+++ Restaurant/Controller/VoucherController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Restaurant.Model;
+
+namespace Restaurant.Controller
+{
+    class VoucherController
+    {
+        private List<Voucher> items;
+
+        public VoucherController()
+        {
+            items = new List<Voucher>();
+        }
+
+        public void addItem(Voucher item)
+        {
+            this.items.Add(item);
+        }
+
+        public List<Voucher> getItems()
+        {
+            return this.items;
+        }
+
+    }
+}
